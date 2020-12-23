b15cef276a7e5425debd12d01bb0f1e001bb362f
CSP CSP <violacsaba@gmail.com>
2020-12-23 20:09:19 +0100

    Commit create versenyzo class

---
+++ LoversenySzimulator/LoversenySzimulator/Entities/versenyzo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoversenySzimulator.Entities
+{
+    class versenyzo
+    {
+        public int LoID { get; set; }
+        public int LoNev { get; set; }
+        public int ZsokeID { get; set; }
+        public int Zsokenev { get; set; }
+        public int Eredmenyek { get; set; }
+
+    }
+}
