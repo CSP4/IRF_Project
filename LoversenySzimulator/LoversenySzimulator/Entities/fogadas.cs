7ee2edec2e03ab4c3cf9a7de1c5fb7b8877d4c47
CSP CSP <violacsaba@gmail.com>
2020-12-23 21:03:53 +0100

    Commit create fogadas class

---
+++ LoversenySzimulator/LoversenySzimulator/Entities/fogadas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoversenySzimulator.Entities
+{
+    class fogadas
+    {
+        public int helyezes { get; set; }
+        public int versenyzoID { get; set; }
+        public int elso { get; set; }
+        public int masodik { get; set; }
+        public int harmadik { get; set; }
+
+    }
+}
