6bcaebe716405f948737773c4b1e041fa3ca65c5
CSP CSP <violacsaba@gmail.com>
2020-12-25 21:15:44 +0100

    Commit make corrections

--- LoversenySzimulator/LoversenySzimulator/Entities/lovacska.cs
+++ LoversenySzimulator/LoversenySzimulator/Entities/lovacska.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,8 +8,39 @@ using System.Windows.Forms;
 
 namespace LoversenySzimulator.Entities
 {
+
     class Lovacska : Label
     {
         public int VersenyzoID { get; set; }
+        public bool Beerte { get; set; }
+        public Lovacska()
+        {
+            AutoSize = false;
+            Width = 30;
+            Height = 19;
+            Paint += Lovacska_Paint;
+            BackColor = Color.Transparent;
+            Beerte = false;
+        }
+
+        private void Lovacska_Paint(object sender, PaintEventArgs e)
+        {
+            Rajz(e.Graphics);
+        }
+
+        public void Rajz(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/lo"+(VersenyzoID+1)+".png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+
+        public void Fut(int szint)
+        {
+            Random rnd = new Random();
+            Left+=rnd.Next(szint)+5;
+        }
     }
+
+
+
 }
